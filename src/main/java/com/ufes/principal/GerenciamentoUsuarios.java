4091c6ec702280d44369c9467c454acffaee647d
Talles <81382968+Talleshts@users.noreply.github.com>
2024-09-12 22:34:59 -0300

    Ajustando build para rodar o projeto

--- src/main/java/com/ufes/principal/GerenciamentoUsuarios.java
+++ src/main/java/com/ufes/principal/GerenciamentoUsuarios.java
@@ -17,28 +17,8 @@ import java.util.List;
  */
 public class GerenciamentoUsuarios {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         System.out.println("Hello World!");
-        UsuarioDAO usuarioDAO = new UsuarioDAO();
-        Usuario usuario = new Usuario("João","123",true,true );
-        //usuarioDAO.insert(usuario);
-        usuario.setNome("Talles");
-        usuario.setId(2);
-
-        usuarioDAO.update(usuario);
-
-        Usuario usuario2 = usuarioDAO.findByID(1);
-        System.out.println(usuario2.toString());
-        usuario2 = usuarioDAO.findByID(2);
-        System.out.println(usuario2.toString());
-
-
-        List<Usuario> usuarios= usuarioDAO.findAll();
-        System.out.println(usuarios.toString());
-
-        usuarioDAO.remove(2);
-        usuarios= usuarioDAO.findAll();
-        System.out.println(usuarios.toString());
     }
 }
